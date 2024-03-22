844eae02944dbca3d360f339c7cbebda179fd4e1
Elias Shemsu <echosierran@gmail.com>
2024-03-22 14:43:21 +0300

    AnimatedVisibility

--- app/src/main/java/com/example/composeanimation1/MainActivity.kt
+++ app/src/main/java/com/example/composeanimation1/MainActivity.kt
@@ -3,13 +3,28 @@ package com.example.composeanimation1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandIn
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
 import com.example.composeanimation1.ui.theme.ComposeAnimation1Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +32,29 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeAnimation1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Column(modifier = Modifier.fillMaxSize()) {
+                    var isVisible by remember{
+                        mutableStateOf(false)
+                    }
+                    
+                    Button(onClick = { isVisible = !isVisible }) {
+                        Text(text = "Toggle")
+                    }
+                    
+                    AnimatedVisibility(
+                        visible = isVisible,
+                        modifier = Modifier.fillMaxWidth().weight(1f),
+                        enter = fadeIn() +
+//                                slideInHorizontally() +
+                                expandVertically(),
+                        exit = fadeOut() +
+//                                slideOutHorizontally() +
+                                shrinkVertically()
+                    ) {
+                        Box(modifier = Modifier.background(Color.Red))
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeAnimation1Theme {
-        Greeting("Android")
     }
 }
